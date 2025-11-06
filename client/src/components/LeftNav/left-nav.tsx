38c64396ead6c57566687a99431c15198da085e9
LukaAlhonen <luka.alhonen@protonmail.com>
2025-11-06 02:00:41 +0200

    made side-nav collapsable

--- client/src/components/LeftNav/left-nav.tsx
+++ client/src/components/LeftNav/left-nav.tsx
@@ -1,6 +1,10 @@
 import styled from "@emotion/styled";
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../../context/use-auth";
+import colors from "../../colors";
+import { useApolloClient } from "@apollo/client/react";
+import { useState } from "react";
+import LogoutButton from "../../containers/LogoutButton/logout-button";
 
 import HomeSVG from "../../assets/icons/home.svg?react";
 import SearchSVG from "../../assets/icons/search.svg?react";
@@ -8,12 +12,13 @@ import ComposeSVG from "../../assets/icons/plus.svg?react";
 import UserSVG from "../../assets/icons/user.svg?react";
 import SignupSVG from "../../assets/icons/user-add.svg?react";
 import LogoutSVG from "../../assets/icons/exit.svg?react";
-import colors from "../../colors";
-import { useApolloClient } from "@apollo/client/react";
-import LogoutButton from "../../containers/LogoutButton/logout-button";
-// import { GET_POEMS } from "../../pages/Poems/poems.graphql";
+import CollapsedSVG from "../../assets/icons/collapsed.svg?react";
 
 const LeftNav = () => {
+  const [collapsed, setCollapsed] = useState(() => {
+    // idiotic hack to persist collapsed state accross page reloads (i do not like this)
+    return localStorage.getItem("collapsed") === "true"
+  });
   const { user, logout } = useAuth();
   const client = useApolloClient();
   const handleLogout = () => {
@@ -21,21 +26,33 @@ const LeftNav = () => {
       logout();
     })
   }
+  const handleCollapse = () => {
+    setCollapsed(prev => {
+      localStorage.setItem("collapsed", String(!prev));
+      return !prev;
+    })
+  }
   return (
-    <NavContainer>
+    <NavContainer collapsed={collapsed}>
+      <CollapseContainer>
+        <CollapseButton onClick={handleCollapse}>
+          {/*{collapsed ? <CollapsedIcon /> : <ExpandedIcon />}*/}
+          <CollapsedIcon />
+        </CollapseButton>
+      </CollapseContainer>
       <TopNav>
         <NavButton data-testid={"home-link"} to="/">
           <HomeIcon />
-          Home
+          {!collapsed && "Home"}
         </NavButton>
         <NavButton data-testid={"search-link"} to="/search">
           <SearchIcon />
-          Search
+          {!collapsed && "Search"}
         </NavButton>
         {user ? (
         <NavButton data-testid={"compose-link"} to="/compose">
           <ComposeIcon />
-          New Poem
+            {!collapsed && "New Poem"}
         </NavButton>
         ): (<></>)}
       </TopNav>
@@ -44,19 +61,19 @@ const LeftNav = () => {
           <>
             <NavButton data-testid={"profile-link"} to={`/author/${user}`}>
               <UserIcon />
-              <UsernameContainer>{user}</UsernameContainer>
+              {!collapsed && <UsernameContainer>{user}</UsernameContainer>}
             </NavButton>
             {/*<NavButton data-testid={"logout-link"} onClick={handleLogout} to="/">*/}
             <LogoutButton onLogout={handleLogout}>
               <LogoutIcon/>
-              Logout
+              {!collapsed && "Logout"}
             </LogoutButton>
             {/*</NavButton>*/}
           </>
         ) : (
           <>
-            <NavButton data-testid={"signup-link"} to="/signup"><SignupIcon />Signup</NavButton>
-            <NavButton data-testid={"login-link"} to="/login"><UserIcon />Login</NavButton>
+              <NavButton data-testid={"signup-link"} to="/signup"><SignupIcon />{!collapsed && "Signup"}</NavButton>
+              <NavButton data-testid={"login-link"} to="/login"><UserIcon />{!collapsed && "Login"}</NavButton>
           </>
         )
         }
@@ -67,40 +84,75 @@ const LeftNav = () => {
 
 export default LeftNav;
 
-const NavContainer = styled.div({
+const NavContainer = styled.div<{ collapsed: boolean }>(({ collapsed }) => ({
   display: "flex",
   flexDirection: "column",
-  position: "sticky",
+  // position: "sticky",
   top: 0,
-  gridArea: "left-nav",
-  alignSelf: "start",
+  alignSelf: "stretch",
   zIndex: 10,
-  padding: "1em 1em 0 1em",
-  // padding: "1em",
-  height: "100%",
-  borderRight: "2px solid gray",
-});
+  // padding: "1em 1em 0 1em",
+  flexGrow: 1,
+  width: collapsed ? "4.5rem" : "12rem",
+  // borderRight: "2px solid gray",
+  border: "0.15rem solid gray",
+  borderRadius: "0.5rem",
+  margin: "0.7rem 0 0.7rem 0.7rem",
+  transition: "width 0.2s ease-in-out",
+}));
 
 const TopNav = styled.div({
   display: "flex",
   flexDirection: "column",
+  width: "100%",
+  alignItems: "center",
+  padding: "0.7em 0.7em 0 0.7em",
 });
 
 const BottomNav = styled.div({
   marginTop: "auto",
   display: "flex",
   flexDirection: "column",
+  width: "100%",
+  alignItems: "center",
+  padding: "0.7em 0.7em 0 0.7em",
 });
+
+const CollapseContainer = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  borderBottom: "0.15rem solid gray",
+  padding: "0.5em 0.5em 0.5rem 0.5em",
+  alignItems: "left"
+})
+
+const CollapseButton = styled.button({
+  textDecoration: "none",
+  color: colors.textEggshell,
+  border: "none",
+  background: "none",
+  alignItems: "center",
+  display: "flex",
+  marginLeft: "0.33rem",
+  "&:hover": {
+    cursor: "pointer",
+    color: colors.wineRed
+  }
+})
 
 const NavButton = styled(NavLink)({
   textDecoration: "none",
-  width: "9em",
+  textWrap: "nowrap",
+  overflow: "hidden",
+  width: "100%",
   color: colors.backgroundBlack,
   background: colors.textEggshell,
   marginBottom: "1em",
   padding: "0.5em",
   display: "flex",
   alignItems: "center",
+  // justifyContent: "center",
   gap: "1em",
   boxSizing: "border-box",
   border: "0.15em solid gray",
@@ -132,6 +184,13 @@ const navImg = {
   transition: "fill 0.15s ease",
   flexShrink: 0,
 };
+
+const CollapsedIcon = styled(CollapsedSVG)({
+  width: "2em",
+  height: "2em",
+  fill: "currentcolor",
+  flexShrink: 0,
+})
 
 // Nav svg image styles
 const HomeIcon = styled(HomeSVG)({
