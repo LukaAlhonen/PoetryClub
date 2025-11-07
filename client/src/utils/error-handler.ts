124e296b5579819b21f1b33e75a25a0beefa199f
LukaAlhonen <luka.alhonen@protonmail.com>
2025-11-07 14:38:30 +0000

    removed debug console.log

--- client/src/utils/error-handler.ts
+++ client/src/utils/error-handler.ts
@@ -1,5 +1,5 @@
-import type { ErrorLike } from "@apollo/client"
-import { notifyError } from "./notify"
+import type { ErrorLike } from "@apollo/client";
+import { notifyError } from "./notify";
 import { useApolloClient } from "@apollo/client/react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/use-auth";
@@ -9,21 +9,28 @@ export const useHandleError = () => {
   const navigate = useNavigate();
   const { logout } = useAuth();
 
-  return ({error, shouldNotify = true }: { error: ErrorLike, shouldNotify?: boolean }) => {
-    console.log("hello2")
+  return ({
+    error,
+    shouldNotify = true,
+  }: {
+    error: ErrorLike;
+    shouldNotify?: boolean;
+  }) => {
     if (error.message === "not authenticated") {
-      console.log("hello3")
       client.clearStore();
       logout();
-      navigate("/login")
-      if(shouldNotify) notifyError(error.message);
-    } else if (error.message === "Poem does not exist" || error.message === "Author does not exist") {
-      if(shouldNotify) notifyError(error.message)
-      navigate("/")
+      navigate("/login");
+      if (shouldNotify) notifyError(error.message);
+    } else if (
+      error.message === "Poem does not exist" ||
+      error.message === "Author does not exist"
+    ) {
+      if (shouldNotify) notifyError(error.message);
+      navigate("/");
     } else {
       // navigate(0)
-      if (shouldNotify) notifyError(error.message)
-      client.refetchObservableQueries()
+      if (shouldNotify) notifyError(error.message);
+      client.refetchObservableQueries();
     }
-  }
-}
+  };
+};
